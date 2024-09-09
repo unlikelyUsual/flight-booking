c18dc7b6ca22bc4be99da9efeee90ff4521c42e3
unlikelyUsual <kumarprabhat308@gmail.com>
2024-09-09 16:18:49 +0530

    Removed dist folder

--- server/dist/types/enum/user.enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserRole = void 0;
-var UserRole;
-(function (UserRole) {
-    UserRole["ADMIN"] = "admin";
-    UserRole["USER"] = "user";
-})(UserRole || (exports.UserRole = UserRole = {}));
